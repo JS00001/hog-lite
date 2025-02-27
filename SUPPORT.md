2c47243355ff04150654cb99bba353393e92259b
JS <49812749+JS00001@users.noreply.github.com>
2025-02-27 07:22:45 -0500

    support

---
+++ SUPPORT.md
@@ -0,0 +1,3 @@
+# Getting Support
+
+If you have questions, need further assistance, want to request a feature, or more, please [open an issue on GitHub](https://github.com/JS00001/hog-lite/issues/new).
--- src/app/main/settings.tsx
+++ src/app/main/settings.tsx
@@ -141,6 +141,10 @@ export default function Settings() {
       <TextInput disabled label="Email" placeholder={user.email} />
       <TextInput disabled label="First Name" placeholder={user.first_name} />
       <TextInput disabled label="Last Name" placeholder={user.last_name} />
+      <TextInput disabled label="API Key" placeholder={maskedApiKey} />
+      <TextInput disabled label="Email" placeholder={user.email} />
+      <TextInput disabled label="First Name" placeholder={user.first_name} />
+      <TextInput disabled label="Last Name" placeholder={user.last_name} />
 
       <Button color="danger" onPress={onLogout}>
         Logout
