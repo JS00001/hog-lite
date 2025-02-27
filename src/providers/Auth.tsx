c204d99f086175a2b0c2dcfbf7cf780164fc0778
JS <49812749+JS00001@users.noreply.github.com>
2025-02-27 07:32:46 -0500

    new build

--- src/providers/Auth.tsx
+++ src/providers/Auth.tsx
@@ -1,13 +1,21 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useRef } from "react";
 
 import useAuthStore from "@/store/auth";
 import { useGetUser } from "@/hooks/api/user";
 
 export default function AuthProvider({ children }: PropsWithChildren) {
+  const initialized = useRef(false);
+
   const query = useGetUser();
   const hasHydrated = useAuthStore((state) => state.hydrated);
 
-  if (!hasHydrated || query.isLoading) {
+  /**
+   * On the first app load, we want to fetch this query if needed (if the user is loggedin)
+   * then, we dont want to return null when fetching again
+   * so we keep track of it already being initialized
+   */
+  if ((!hasHydrated || query.isLoading) && !initialized.current) {
+    initialized.current = true;
     return null;
   }
 
